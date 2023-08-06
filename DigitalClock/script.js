78e10e9fbd5e1a5556ceab3e01628fffeddbe378
David Agor <agordavid14@gmail.com>
2023-08-06 07:43:15 +0100

    add clock functionality

---
+++ DigitalClock/script.js
@@ -0,0 +1,24 @@
+// return an element with an id using getElementBy
+
+const hourE1 = document.getElementById("hour");
+const minuteE1 = document.getElementById("minute");
+const secondsE1 = document.getElementById("seconds");
+const ampmE1 = document.getElementById("ampm");
+
+
+function updateTime() {
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+if(h > 12){
+    h = h - 12;
+    ampm = "PM";
+}
+
+hourE1.innerText = h;
+minuteE1.innerText = m;
+secondsE1.innerText = s;
+}
+updateTime();
