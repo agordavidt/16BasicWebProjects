58fb97623e79143d631aa1f33a77559e1af57e66
David Agor <agordavid14@gmail.com>
2023-08-12 11:19:48 +0100

    update script.js ;

--- MultipliicationApp/script.js
+++ MultipliicationApp/script.js
@@ -8,17 +8,30 @@ questionEl.innerText = `What is ${num1} multiply by ${num2}`;
 const inputEl = document.getElementById("input");
 const formEl = document.getElementById("form");
 
+
+
+let score = JSON.parse(localStorage.getItem("score"));
+
 const correctAns = num1 * num2;
 // add invent listerner to the html form to track submissions
 // add event listener to the form (submit)
 // create a call back function to listen for event (envent listenner)
 
 formEl.addEventListener("submit", ()=>{
-    const userAns = inputEl.value;
-    console.log(userAns, typeof userAns);
-
-    
+    const userAns = +inputEl.value;  // get information inside input
+    if(userAns === correctAns){
+        score++;
+    }else{
+        score--;
+        updateLocalStorage(); 
+    }   
 })
+
+
+// a function to store scores in the local storage of the browser
+function updateLocalStorage(){
+    localStorage .setItem("score", JSON.stringify(score))
+}
 
 
 
