668b5c50429382598f05f5257ec87d3a657b0707
David Agor <agordavid14@gmail.com>
2023-11-11 01:09:15 +0100

    remove script file

--- Random-Color-Generator/script.js
+++
@@ -1,9 +0,0 @@
-const containerEl = document.querySelector(".container");
-
-for(let index = 0; index < 30; index++){
-    const colorContainerEl = document.createElement("div");
-
-    colorContainerEl.classList.add("color_container");
-
-    containerEl.appendChild(colorContainerEl);
-}
